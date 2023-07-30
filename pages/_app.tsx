b23feefbf1234f415d88dba7ab77e58b2c87c5ee
nurdinahmadalawiyah <nurdinahmada@gmail.com>
2023-07-30 17:00:08 +0700

    fix push notification

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,59 +1,48 @@
-import '../styles/globals.css';
-import type {AppProps} from 'next/app';
-import {createTheme, NextUIProvider} from '@nextui-org/react';
-import {ThemeProvider as NextThemesProvider} from 'next-themes';
-import {Layout} from '../components/layout/layout';
-import { useEffect } from 'react';
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { createTheme, NextUIProvider } from "@nextui-org/react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { Layout } from "../components/layout/layout";
+import { useEffect } from "react";
+import OneSignalReact from "react-onesignal";
 
 const lightTheme = createTheme({
-   type: 'light',
-   theme: {
-      colors: {},
-   },
+  type: "light",
+  theme: {
+    colors: {},
+  },
 });
 
 const darkTheme = createTheme({
-   type: 'dark',
-   theme: {
-      colors: {},
-   },
+  type: "dark",
+  theme: {
+    colors: {},
+  },
 });
 
-function MyApp({Component, pageProps}: AppProps) {
-   useEffect(() => {
-      if (typeof window !== undefined) {
-         window.OneSignal = window.OneSignal || [];
-         OneSignal.push(function () {
-            OneSignal.init({
-               appId: "87cf8313-c7f7-420a-b2dd-fbf5a3e29513",
-               notifyButton: {
-                  enable: true,
-               },
-            });
-         });
-      }
+function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    OneSignalReact.init({
+      appId: "87cf8313-c7f7-420a-b2dd-fbf5a3e29513",
+    });
+  }, []);
 
-      return () => {
-         window.OneSignal = undefined;
-      };
-   }, []);
-
-   return (
-      <NextThemesProvider
-         defaultTheme="light"
-         attribute="class"
-         value={{
-            light: lightTheme.className,
-            dark: darkTheme.className,
-         }}
-      >
-         <NextUIProvider>
-            <Layout>
-               <Component {...pageProps} />
-            </Layout>
-         </NextUIProvider>
-      </NextThemesProvider>
-   );
+  return (
+    <NextThemesProvider
+      defaultTheme="light"
+      attribute="class"
+      value={{
+        light: lightTheme.className,
+        dark: darkTheme.className,
+      }}
+    >
+      <NextUIProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </NextUIProvider>
+    </NextThemesProvider>
+  );
 }
 
 export default MyApp;
