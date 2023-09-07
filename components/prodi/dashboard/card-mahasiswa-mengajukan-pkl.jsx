35105746d5cf856a1758b65b3387b480a5a35fde
nurdinahmadalawiyah <nurdinahmada@gmail.com>
2023-09-07 23:06:26 +0700

    menambah informasi pada dashboard prodi

---
+++ components/prodi/dashboard/card-mahasiswa-mengajukan-pkl.jsx
@@ -0,0 +1,34 @@
+import { Card, Text, Col, Row, Spacer, Loading} from "@nextui-org/react";
+import { Flex } from "../../styles/flex";
+import React from "react";
+
+export const CardMahasiswaMengajukanPkl = ({data}) => {
+  return (
+    <Card
+      css={{
+        borderRadius: "$xl",
+        flex: "1 1 100%",
+        marginBottom: "10px",
+        justifyContent: "center",
+      }}
+    >
+      <Card.Body>
+        <Flex>
+          <Col>
+            <Row>
+              <Text b size={20} css={{ tt: "capitalize", textAlign: "center", width: "100%" }}>
+                Mahasiswa Yang Sudah Mengajukan PKL
+              </Text>
+            </Row>
+            <Spacer y={2} />
+            <Row css={{ textAlign: "center", width: "100%" }}>
+              <Text b size={28} css={{ tt: "capitalize", width: "100%" }}>
+              {data && data.mahasiswa_sedang_pkl ? `${data.mahasiswa_sedang_pkl} Orang` : <Loading size="xl" color="success" />}
+              </Text>
+            </Row>
+          </Col>
+        </Flex>
+      </Card.Body>
+    </Card>
+  );
+};
--- components/prodi/dashboard/index.jsx
+++ components/prodi/dashboard/index.jsx
@@ -1,50 +1,96 @@
-import React, { useEffect } from 'react';
-import {Flex} from '../../styles/flex';
-import { Box } from '../../styles/box';
-import { CardWelcomeProdi } from './card-welcome-prodi';
-import { useRouter } from 'next/router';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useState, useEffect} from "react";
+import { Flex } from "../../styles/flex";
+import { Box } from "../../styles/box";
+import { CardWelcomeProdi } from "./card-welcome-prodi";
+import { CardTotalMahasiswaProdi } from "./card-total-mahasiswa-prodi";
+import { CardMahasiswaMengajukanPkl } from "./card-mahasiswa-mengajukan-pkl";
+import { CardMahasiswaTelahPkl } from "./card-mahasiswa-telah-pkl";
+import { CardMahasiswaBelumPkl } from "./card-mahasiswa-belum-pkl";
+import { useRouter } from "next/router";
+import axios from "axios";
 
 export const DashboardProdi = () => {
-   const router = useRouter();
+  const router = useRouter();
+  const [data, setData] = useState();
 
-   useEffect(() => {
-      const accessToken = localStorage.getItem('accessToken');
-      const role = localStorage.getItem('role');
+  useEffect(() => {
+    const accessToken = localStorage.getItem("accessToken");
+    const role = localStorage.getItem("role");
 
-      if (!accessToken || role !== "Prodi") {
-         router.push('/login-prodi');
-      }
-   })
+    if (!accessToken || role !== "Prodi") {
+      router.push("/login-prodi");
+    }
+  });
 
-   return (
-      <Flex
-         css={{
-            'mt': '$5',
-            'px': '$6',
-            '@sm': {
-               mt: '$10',
-               px: '$16',
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(
+        `${process.env.API_BASE_URL}/tempat-pkl/prodi/data`,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+          },
+        }
+      );
+      setData(response.data.data)
+      console.log(response.data.data);
+      console.log(data.total_mahasiswa);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <Flex
+      css={{
+        mt: "$5",
+        px: "$6",
+        "@sm": {
+          mt: "$10",
+          px: "$16",
+        },
+      }}
+      justify={"center"}
+      direction={"column"}
+    >
+      <Box>
+        <Flex
+          css={{
+            gap: "$10",
+            flexWrap: "wrap",
+            justifyContent: "center",
+            "@sm": {
+              flexWrap: "nowrap",
             },
-         }}
-         justify={'center'}
-         direction={'column'}
-      >
+          }}
+          direction={"column"}
+        >
+          <CardWelcomeProdi />
+          <Flex
+            css={{
+              gap: "$10",
+              flexWrap: "wrap",
+              justifyContent: "center",
+              "@sm": {
+                flexWrap: "nowrap",
+              },
+            }}
+            direction={"row"}
+          >
+            <CardTotalMahasiswaProdi data={data} />
+            <CardMahasiswaMengajukanPkl data={data} />
+            <CardMahasiswaTelahPkl data={data} />
+            <CardMahasiswaBelumPkl data={data} />
 
-         <Box>
-               <Flex
-                  css={{
-                     'gap': '$10',
-                     'flexWrap': 'wrap',
-                     'justifyContent': 'center',
-                     '@sm': {
-                        flexWrap: 'nowrap',
-                     },
-                  }}
-                  direction={'row'}
-               >
-                  <CardWelcomeProdi />
-               </Flex>
-            </Box>
-      </Flex>
-   );
+          </Flex>
+        </Flex>
+      </Box>
+    </Flex>
+  );
 };
