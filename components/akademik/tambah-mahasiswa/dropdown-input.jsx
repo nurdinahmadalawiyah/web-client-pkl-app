a3af132a49829a1e1a286d8a8b414cbf85556e24
nurdinahmadalawiyah <nurdinahmada@gmail.com>
2023-09-08 22:37:28 +0700

    fix add mahasiswa

--- components/akademik/tambah-mahasiswa/dropdown-input.jsx
+++ components/akademik/tambah-mahasiswa/dropdown-input.jsx
@@ -6,9 +6,6 @@ export const DropdownInput = ({
   prodi,
   setProdi,
   handleProdiChange,
-  semester,
-  setSemester,
-  handleSemesterChange,
 }) => {
   const [data, setData] = useState([]);
   const [serverError, setServerError] = useState(false);
@@ -34,8 +31,6 @@ export const DropdownInput = ({
   }, []);
   
   return (
-    <Grid.Container>
-      <Grid xs={6}>
         <Dropdown>
           <Dropdown.Button
             flat
@@ -66,35 +61,5 @@ export const DropdownInput = ({
             )}
           </Dropdown.Menu>
         </Dropdown>
-      </Grid>
-      <Grid xs={6}>
-        <Dropdown>
-          <Dropdown.Button
-            flat
-            color="primary"
-            css={{ tt: "capitalize", width: "100%" }}
-          >
-            {handleSemesterChange}
-          </Dropdown.Button>
-          <Dropdown.Menu
-            aria-label="Drop Down Semester"
-            color="primary"
-            disallowEmptySelection
-            selectionMode="single"
-            selectedKeys={semester}
-            onSelectionChange={setSemester}
-          >
-            <Dropdown.Item key="1 (Satu)">1 (Satu)</Dropdown.Item>
-            <Dropdown.Item key="2 (Dua)">2 (Dua)</Dropdown.Item>
-            <Dropdown.Item key="3 (Tiga)">3 (Tiga)</Dropdown.Item>
-            <Dropdown.Item key="4 (Empat)">4 (Empat)</Dropdown.Item>
-            <Dropdown.Item key="5 (Lima)">5 (Lima)</Dropdown.Item>
-            <Dropdown.Item key="6 (Enam)">6 (Enam)</Dropdown.Item>
-            <Dropdown.Item key="7 (Tujuh)">7 (Tujuh)</Dropdown.Item>
-            <Dropdown.Item key="8 (Delapan)">8 (Delapan)</Dropdown.Item>
-          </Dropdown.Menu>
-        </Dropdown>
-      </Grid>
-    </Grid.Container>
   );
 };
